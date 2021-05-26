d5a09d85dcf84f80333fc6d4bd69e27300bba119
ubercj <ubercj@gmail.com>
2021-05-26 19:31:28 -0400

    Added github logo and link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../styles/Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Navbar = () => {
 
   return (
     <nav className="Navbar">
       <h1>CV Builder</h1>
+      <a href="https://github.com/ubercj" className="github"><FontAwesomeIcon icon={faGithub} /></a>
     </nav>
   )
 }
